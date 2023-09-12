10cd522994c52931b861bf36624f82fcffbf56ae
stellarboymihir <stellarboymihir00@gmail.com>
2023-09-13 02:36:24 +0530

    Sliver App Bar

---
+++ silver_app_bar/lib/homepage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: CustomScrollView(
+        slivers: [
+          // SLiver App Bar
+          SliverAppBar(
+            backgroundColor: Colors.deepPurple,
+            leading: const Icon(Icons.menu),
+            // title: Text('S L I V E R A P P B A R'),
+            expandedHeight: 300,
+            floating: true,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Colors.pink,
+              ),
+              title: const Text('S L I V E R  A P P  B A R'),
+            ),
+          ),
+
+          // SLiver Items
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 400,
+                  color: Colors.deepPurple[300],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 400,
+                  color: Colors.deepPurple[300],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 400,
+                  color: Colors.deepPurple[300],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
